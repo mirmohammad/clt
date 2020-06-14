37f7f136e8f69348bf3c94539930e5c1e19c8ace
Mirmohammad Saadati <mir.mohammad.saadati@gmail.com>
2020-06-14 13:48:51 -0400

    who knows right ??

--- toreno1.sh
+++ toreno1.sh
@@ -2,16 +2,16 @@
 
 echo
 echo ">>>>>>>>>>>>>>>>>>> START"
-python main.py /data/Databases/mir/s4/ -b 32 -e 50 -g 0 --step_lr --skd_step 25
+python main.py /data/Databases/mir/s4/ -b 32 -e 50 -g 1 --step_lr --skd_step 25
 
 echo
 echo ">>>>>>>>>>>>>>>>>>> START"
-python main.py /data/Databases/mir/s4/ -b 32 -e 50 -g 0 --step_lr --skd_step 25 --vflip
+python main.py /data/Databases/mir/s4/ -b 32 -e 50 -g 1 --step_lr --skd_step 25 --vflip
 
 echo
 echo ">>>>>>>>>>>>>>>>>>> START"
-python main.py /data/Databases/mir/s4/ -b 32 -e 50 -g 0 --step_lr --skd_step 25 --hflip
+python main.py /data/Databases/mir/s4/ -b 32 -e 50 -g 1 --step_lr --skd_step 25 --hflip
 
 echo
 echo ">>>>>>>>>>>>>>>>>>> START"
-python main.py /data/Databases/mir/s4/ -b 32 -e 50 -g 0 --step_lr --skd_step 25 --vflip --hflip
+python main.py /data/Databases/mir/s4/ -b 32 -e 50 -g 1 --step_lr --skd_step 25 --vflip --hflip
